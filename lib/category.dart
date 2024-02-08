f18b2333c226d80605d6e264cd222f043f2ff0c4
evixture <pixture1311@gmail.com>
2024-02-08 08:35:24 -0600

    getting ready to write to yaml file

--- lib/category.dart
+++ lib/category.dart
@@ -20,6 +20,10 @@ class _CategoryState extends State<Category> {
   List<NoteCoreData> ncdList = [];
   String catName = '';
 
+  bool textChanged = false;
+  bool submittedText = false;
+  bool clickedOff = false;
+
   Future<void> readYAML(String path) async {
     var resp = await rootBundle.loadString(path);
     YamlMap yaml = loadYaml(resp);
@@ -65,8 +69,42 @@ class _CategoryState extends State<Category> {
           child: Row(
             children: [
               Expanded(
-                child: TextField(
-                  decoration: InputDecoration(labelText: catName),
+                child: EditableText(
+                  controller: TextEditingController(text: "SOMETHING"),
+                  onSubmitted: (value) {
+                    //print("submitted, $value");
+                    catName = value;
+                    textChanged = false;
+                    print("saved to file");
+                    //yamlUpdate();
+                  },
+                  // onEditingComplete: () {
+                  //   print("editing complete");
+                  // },
+                  onChanged: (value) {
+                    //print('changed text, $value');
+                    catName = value;
+                    textChanged = true;
+                  },
+                  onTapOutside: (event) {
+                    //print("clicked off, $event");
+
+                    if (textChanged) {
+                      textChanged = false;
+                      print("saved to file");
+                      //yamlUpdate();
+                    }
+                  },
+                  focusNode: FocusNode(
+                    canRequestFocus: true,
+                  ),
+                  backgroundCursorColor: Colors.grey,
+                  cursorColor: Colors.white,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
               FloatingActionButton(
